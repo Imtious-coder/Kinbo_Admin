22792e83d877dbe6b406e1bfb64ac1743cc6e7cf
Imtious-Coder <iarafat386@gmail.com>
2024-05-29 17:42:14 +0600

    :art: Product adding to database is working.

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -5,31 +5,108 @@ import upload_area from "../assets/upload_area.svg";
 
 const AddProduct = () => {
   const [image, setImage] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    image: "",
+    category: "women",
+    new_price: "",
+    old_price: "",
+  });
 
+  // SELECTED IMAGE DISPLAY FUNCTION
   const imageHandler = (e) => {
     setImage(e.target.files[0]);
     console.log(image);
+  };
+
+  const chnageHandler = (e) => {
+    setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+  };
+
+  const addProduct = async () => {
+    console.log({productDetails});
+    let responseData;
+    let product = productDetails;
+
+    let formData = new FormData();
+    formData.append("product", image);
+
+    await fetch("http://localhost:4000/upload", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        responseData = data;
+      });
+    if (responseData.success) {
+      product.image = responseData.image_url;
+      console.log({product});
+      await fetch("http://localhost:4000/addproduct", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          data.success
+            ? alert("Product Added Successfully")
+            : alert("Product Added Successfully");
+        });
+    }
   };
 
   return (
     <section className="addproduct">
       <div className="addproduct-itemfield">
         <p>Product Title</p>
-        <input type="text" name="name" id="" placeholder="Type Here" />
+        <input
+          value={productDetails.name}
+          onChange={chnageHandler}
+          type="text"
+          name="name"
+          id=""
+          placeholder="Type Here"
+        />
       </div>
       <div className="addproduct-price">
         <div className="addproduct-itemfield">
           <p>Price</p>
-          <input type="text" name="old_price" id="" placeholder="Type Here" />
+          <input
+            value={productDetails.old_price}
+            onChange={chnageHandler}
+            type="text"
+            name="old_price"
+            id=""
+            placeholder="Type Here"
+          />
         </div>
         <div className="addproduct-itemfield">
           <p>Offer Price</p>
-          <input type="text" name="new_price" id="" placeholder="Type Here" />
+          <input
+            value={productDetails.new_price}
+            onChange={chnageHandler}
+            type="text"
+            name="new_price"
+            id=""
+            placeholder="Type Here"
+          />
         </div>
       </div>
       <div className="addproduct-itemfield">
         <p>Product Category</p>
-        <select name="category" className="addproduct-selector">
+        <select
+          value={productDetails.category}
+          onChange={chnageHandler}
+          name="category"
+          className="addproduct-selector"
+        >
           <option value="women">Women</option>
           <option value="men">Men</option>
           <option value="kid">Kid</option>
@@ -51,7 +128,14 @@ const AddProduct = () => {
           hidden
         />
       </div>
-      <button className="addproduct-btn">ADD</button>
+      <button
+        onClick={() => {
+          addProduct();
+        }}
+        className="addproduct-btn"
+      >
+        ADD
+      </button>
     </section>
   );
 };
